9e34855174c47863bdd60287296b1f11c243926d
olivera_julieta@outlook.com <olivera_julieta@outlook.com>
2023-02-13 18:17:10 -0300

    Subiendo portfolio

--- validacion.js
+++ validacion.js
@@ -1,1 +1,23 @@
 //Haz tú validación en javascript acá
+const enviarMensaje = () => {
+    console.log();
+  
+    let mail = document.createElement("a");
+    let asunto = document.getElementById("asunto").value;
+    let Mensaje = document.getElementById("mensaje").value;
+  
+    asunto = asunto.replace(/ /gi, "%20");
+    Mensaje = Mensaje.replace(/ /gi, "%20");
+  
+    referencia =
+      "mailto:olivera_julieta@outlook.com?subject=" +
+      asunto +
+      "&body=" +
+      Mensaje;
+    mail.href = referencia;
+    mail.click();
+  };
+  
+  let button = document.getElementById("btnenviar");
+  button.addEventListener("click", enviarMensaje);
+  
